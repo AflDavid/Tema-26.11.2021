e2a1eb075fa58090820b909165903758d4fdaab4
AflDavid <afloareidavid@gmail.com>
2021-11-25 23:55:36 +0200

    Add project files.

---
+++ vector1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace vector1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[100];
+            int n, s = 0, nrp = 0;
+            int minn = int.MaxValue;
+            int maxx = int.MinValue;
+            double med;
+
+            n = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                v[i] = Int32.Parse(Console.ReadLine());
+                s = s + v[i];
+                if (v[i] % 2 == 0) nrp++;
+                if (v[i] < minn) minn = v[i];
+                if (v[i] > maxx) maxx = v[i];
+            }
+            double sum = Convert.ToDouble(s);
+            double nr = Convert.ToDouble(n);
+            med = sum / n;
+
+            Console.WriteLine("sum= " + s);
+            Console.WriteLine("avr= " + med);
+            Console.WriteLine("nr valori pare= " + nrp);
+            Console.WriteLine("min= " + minn);
+            Console.WriteLine("max= " + maxx);
